423164ccff26d63f33f3821a1b63ab4c11278b3b
yohoo217 <124127737+yohoo217@users.noreply.github.com>
2023-02-23 15:01:12 +0800

    Initial Commit

---
+++ test_with_ai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_with_ai
+//
+//  Created by aotter on 2023/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
